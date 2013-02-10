6b1d6f97ff7348ecab14e2970cf33eeab29aff04
gundi <hock@zhaw.ch>
2013-02-10 20:26:16 +0100

    added AsSignal implementation for ColorPicker

--- ui/src/main/scala/efa/cf/ui/package.scala
+++ ui/src/main/scala/efa/cf/ui/package.scala
@@ -1,11 +1,13 @@
 package efa.cf
 
+import efa.cf.format._
+import efa.cf.ui.spi.UiLocal
 import efa.core.{Service, ValSt, Efa}, Efa._
 import efa.nb.dialog.{DialogEditable ⇒ DE}
 import efa.nb.node.NodeOut
-import efa.cf.format._
-import efa.cf.ui.spi.UiLocal
+import efa.react.{AsSignal}
 import java.awt.Color
+import java.beans.{PropertyChangeListener, PropertyChangeEvent}
 import org.efa.gui.ColorPicker
 import scalaz.effect.IO
 
@@ -28,6 +30,23 @@ package object ui {
     IO(ColorPicker.showDialog(null, c, true))
 
   private[ui] lazy val pickColorWhite: IO[Color] = pickColor(Color.WHITE)
+
+  private val ColCh = ColorPicker.SELECTED_COLOR_PROPERTY
+  private val OpCh = ColorPicker.OPACITY_PROPERTY
+
+  implicit val ColorPanelAsSignal = AsSignal.unit[ColorPicker,Color] (
+    (cp, f) ⇒  cp.addPropertyChangeListener(
+      new PropertyChangeListener {
+        def propertyChange(e: PropertyChangeEvent) {
+          e.getPropertyName match {
+            case ColCh ⇒ f(cp.getColor)
+            case OpCh  ⇒ f(cp.getColor)
+            case _     ⇒ ()
+          }
+        }
+      }
+    ), _.getColor
+  )
 }
 
 // vim: set ts=2 sw=2 et:
