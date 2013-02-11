6fcee15eaf401729b1db87c80445713e8642f87c
gundi <hock@zhaw.ch>
2013-02-11 07:12:06 +0100

    more on GradientColorsNode

--- ui/src/main/scala/efa/cf/ui/ColorPanel.scala
+++ ui/src/main/scala/efa/cf/ui/ColorPanel.scala
@@ -4,9 +4,10 @@ import efa.core.Efa._
 import efa.react.swing.{GbPanel, Swing}, Swing._
 import efa.nb.VSIn
 import java.awt.Color
+import org.efa.gui.ColorPicker
 import scala.swing.{Button, BorderPanel}
 import scala.swing.GridBagPanel.Fill
-import scalaz._, Scalaz._
+import scalaz._, Scalaz._, effect.IO
 
 class ColorPanel (c: Color) extends BorderPanel {
   val btn = new Button("...")
@@ -14,10 +15,15 @@ class ColorPanel (c: Color) extends BorderPanel {
   opaque = true
 
   lazy val colorIn: VSIn[Color] =
-    backgroundS(this) on btn mapIO pickColor hold c to 
+    backgroundS(this) on btn mapIO ColorPanel.pickColor hold c to 
     Swing.background(this) map (_.success)
 
   add(btn, BorderPanel.Position.East)
+}
+
+object ColorPanel {
+  private[ui] def pickColor (c: Color): IO[Color] =
+    IO(ColorPicker.showDialog(null, c, true))
 }
 
 // vim: set ts=2 sw=2 et:
--- ui/src/main/scala/efa/cf/ui/GradientColorsNode.scala
+++ ui/src/main/scala/efa/cf/ui/GradientColorsNode.scala
@@ -1,35 +1,32 @@
 package efa.cf.ui
 
-import efa.core.ValSt
+import efa.core.{ValSt, Efa}, Efa._
 import efa.cf.format._, efa.cf.format.{loc ⇒ fLoc}
 import efa.nb.node.{NbNodeFunctions, NodeOut, NbChildrenFunctions}
 import java.awt.Color
 import scalaz._, Scalaz._, effect._
 
 object GradientColorsNode extends NbNodeFunctions with NbChildrenFunctions {
-//  type FFG = FullFormat[GradientColors]
-//  type CSI = (Color,String,Int)
-//
-//  lazy val l = Lens.self[AllFormats]
-//
-//  lazy val rootOut: NodeOut[AllFormats,ValSt[AllFormats]] =
-//    children(uniqueIdF(ffgOut)((_: AllFormats).gradientColorsFF)) ⊹
-//    clearNt ⊹ 
-//    (addNtDialogEs(l.addGCs) ∙ (_.gcBluePrint)) ⊹
-//    nameA(loc.gradients) ⊹
-//    contextRootsA(List("ContextActions/GradientsNode")) ⊹
-//    iconBaseA("efa/cf/ui/format.png")
-//
-//  lazy val ffgOut: NodeOut[FFG,ValSt[AllFormats]] =
-//    children(leavesF(colorOut)(colorNameIdx)) ⊹
-//    destroyEs(l delGCs _.format.name) ⊹ 
-//    (editDialog[FFG,GradientColors] withIn (l.updateGCs(_,_).success)) ⊹
-//    name(_.format.name) ⊹
-//    contextRootsA(List("ContextActions/SingleGradientNode")) ⊹
-//    iconBaseA("efa/cf/ui/format.png") ⊹
-//    clearNt ⊹
-//    addColor
-//
+
+  //@TODO add add
+  lazy val rootOut: AfOut[AfRoot] =
+    (clearNt: AfOut[AfRoot]) ⊹ 
+    children(parentNamedF(gcOut)) ⊹
+    //addNtDialogP ⊹
+    nameA(loc.gradients) ⊹
+    contextRootsA(List("ContextActions/GradientsNode")) ⊹
+    iconBaseA("efa/cf/ui/format.png")
+
+  lazy val gcOut: AfOut[GcPath] =
+    (named[GcPath]: AfOut[GcPath]) ⊹ 
+    children(parentF(colorOut)) ⊹
+    destroyP ⊹
+    editDialogP ⊹
+    contextRootsA(List("ContextActions/SingleGradientNode")) ⊹
+    iconBaseA("efa/cf/ui/format.png") ⊹
+    clearNt ⊹
+    createNtDialogP[List,AllFormats,IdColor,GcPath,Int]{_ ⇒ (-1,Color.WHITE)}
+
   lazy val colorOut: AfOut[FullColor] =
     (destroyP: AfOut[FullColor]) ⊹ 
     ColorEditor.colorA[FullColor](_.head._2) ⊹
